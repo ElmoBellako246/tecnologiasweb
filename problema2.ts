928dbfde2ee946e792b58732c2917e203cd1ddb3
JuanPablo_CR7 <323030315@upjr.edu.mx>
2025-05-20 08:44:58 -0600

    Create problema2.ts

---
+++ problema2.ts
@@ -0,0 +1,8 @@
+(()=>{
+    const addTwoNumbers = (params) => {
+    return params.first + params.second;
+    };
+  console.log(addTwoNumbers({first: 2, second: 4}) === 6);
+
+  console.log(addTwoNumbers({first: 10,second: 20}) === 30);
+})()
